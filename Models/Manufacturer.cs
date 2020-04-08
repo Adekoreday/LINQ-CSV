42e543381b09ee2bce8c4fcfc829b705c6fdc759
Adekoreday <adekoredead@ha-shem.com>
2020-04-08 05:30:24 +0100

    modularize csv reader

---
+++ Models/Manufacturer.cs
@@ -0,0 +1,13 @@
+using System;
+
+ namespace Models
+{
+    public class Manufacturers 
+   {
+     public string Name {get; set;}
+     public string HeadQuaters {get; set; }
+
+     public int Year {get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,28 +10,45 @@ namespace csv_Reader
     {
         static void Main(string[] args)
         {
-            var allCar = ProcessCsv("fuel.csv");
+            var allCar = ProcessCsv<Car>("fuel.csv", ProcessCar);
+            var allManufacturers = ProcessCsv<Manufacturers>("manufacturers.csv", ProcessManufacturers);
+
             foreach (var item in allCar) {
+                Console.WriteLine(item.Name);
+            }
+                Console.WriteLine("Reading Manufacturers <--------->");
+            foreach(var item in allManufacturers) {
                 Console.WriteLine(item.Name);
             }
         }
 
-         public static Models.Car  ProcessLines (string Line) {
+        public static Manufacturers ProcessManufacturers (string Line) {
             var contents = Line.Split(',');
             Console.WriteLine("cases", contents[0]);
-            return new Models.Car () {
+            return new Manufacturers () {
+                Name = contents[0],
+                HeadQuaters = contents[1],
+                Year = int.Parse(contents[2])
+            };
+        }
+         public static Car  ProcessCar (string Line) {
+            var contents = Line.Split(',');
+            Console.WriteLine("cases", contents[0]);
+            return new Car () {
                 Name = contents[1],
                 Manufacturer = contents[2],
                 Displacement = double.Parse(contents[3]),
                 Cyllinders = int.Parse(contents[4])
             };
         }
-        public static IEnumerable<Models.Car > ProcessCsv (string path) {
+        public static IEnumerable<T> ProcessCsv<T> (string path, Func<string, T> ProcessItem) {
             return File.ReadAllLines(path)
             .Skip(1)
             .Where(lines => lines.Length > 1)
-            .Select(item => ProcessLines(item))
+            .Select(item => ProcessItem(item))
             .ToList();
         }
+
+
     }
 }
